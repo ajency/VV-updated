31631f14407c6d20c5346dd6e14edaad8696f4a8
Jarryd <jarryd@ajency.in>
2013-03-21 18:47:13 +0530

    Commit

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'videovolunteersajency');
+define('DB_NAME', 'vv_updated');
 
 /** MySQL database username */
-define('DB_USER', 'jessicavv');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'mayberry@1');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql.vv.ajency.in');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
