dc95b88c8057ad48273b529f40b6f68f1c8ab451
Amit Adav <amit@ajency.in>
2013-04-12 11:45:35 +0530

    contact form added

--- wp-content/themes/video-volunteers/functions.php
+++ wp-content/themes/video-volunteers/functions.php
@@ -430,6 +430,45 @@ function output_single_author_bio() {
 							<a href="#"><img src="<?php bloginfo('url'); ?>/wp-content/uploads/2013/02/connect-twitter.png" alt="connect-twitter" width="16" height="16" class="alignnone size-full wp-image-234" /></a>
 							<a href="mailto:<?php echo get_the_author_meta('email'); ?>"><img src="<?php bloginfo('url'); ?>/wp-content/uploads/2013/02/connect-email.png" alt="connect-email" width="16" height="16" class="alignnone size-full wp-image-232" /></a>
 						</span>
+						<?php 
+if ($_POST["email"]<>'') { 
+    $ToEmail = $_POST[_post_author_email];
+    $EmailSubject = 'Site contact form'; 
+    $mailheader = "From: ".$_POST["email"]."\r\n"; 
+    $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+    $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+    $MESSAGE_BODY = "Name: ".$_POST["name"].""; 
+    $MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
+    $MESSAGE_BODY .= "Comment: ".nl2br($_POST["comment"]).""; 
+    mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
+?> 
+Your message was sent
+<?php 
+} else { 
+?> 
+<form action="" method="post">
+<table width="400" border="0" cellspacing="2" cellpadding="0">
+<tr>
+<td width="29%" class="bodytext">Your name:</td>
+<td width="71%"><input name="name1" type="text" id="name" size="32"></td>
+</tr>
+<tr>
+<td class="bodytext">Email address:</td>
+<td><input name="email1" type="text" id="email" size="32"></td>
+</tr>
+<tr>
+<td class="bodytext">Comment:</td>
+<td><textarea name="comment1" cols="45" rows="6" id="comment" class="bodytext"></textarea></td>
+</tr>
+<tr>
+<td class="bodytext"> </td>
+<td align="left" valign="top"><input type="submit" name="Submit" value="Send"></td>
+</tr>
+</table>
+</form> 
+<?php 
+}; 
+?>
 						<div class="auth-video">
 							<?php
 							$video = $curauth->wpum_video_profile;
